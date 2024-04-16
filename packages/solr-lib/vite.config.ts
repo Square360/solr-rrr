f764c38df5b09744b8e2361fa5619c361e4f9d44
Douglas McGilvray <dm@studio-fas.io>
2024-04-16 11:06:40 -0300

    Add types & module definition

--- packages/solr-lib/vite.config.ts
+++ packages/solr-lib/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'SolrLib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `solr-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
