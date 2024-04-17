9aad5dd40bd6038cadabe2754df0b5086fc8a228
Douglas McGilvray <dm@studio-fas.io>
2024-04-17 18:23:39 -0300

    chore: Rmove unused

--- sites/solr-test/src/main.tsx
+++ sites/solr-test/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { store } from './store/store';
